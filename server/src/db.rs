162bce7be38d21aa47d7e7e889c7cfa9ca185f08
nyuta <nyuta.main@gmail.com>
2022-11-02 06:54:31 +0000

    ✨ get job data from db

--- server/src/db.rs
+++ server/src/db.rs
@@ -1,12 +1,24 @@
-use diesel::pg::PgConnection;
+use super::models::TradeJob;
+use super::*;
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
 
-pub fn establish_connection() -> PgConnection {
+pub fn establish_connection() -> MysqlConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
+    MysqlConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn get_jobs(con: &mut MysqlConnection) -> Vec<TradeJob> {
+    use self::schema::trade_jobs::dsl::*;
+
+    let results = trade_jobs
+        .filter(status.eq(true))
+        .limit(5)
+        .load::<TradeJob>(con)
+        .expect("err");
+    results
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,14 @@
+mod db;
+pub mod models;
+pub mod schema;
 
-fn main() {
-    println!("Hello, world!");
+pub fn main() {
+    println!("Hello");
+
+    let con = &mut db::establish_connection();
+
+    let jobs = db::get_jobs(con);
+    for job in jobs {
+        println!("{}", job.id);
+    }
 }
