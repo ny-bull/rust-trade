771a0d1b09b52eaa2377412ee5028369bf8e13a6
nyuta <nyuta.main@gmail.com>
2022-11-06 23:18:30 +0000

    ✨ introduce http client

---
+++ server/src/http/binance.rs
@@ -0,0 +1,3 @@
+pub fn order(job:TradeJob){
+    
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,15 @@
-mod db;
-pub mod models;
-pub mod schema;
+pub mod database;
+
+use database::*;
 
 pub fn main() {
     println!("Hello");
 
-    let con = &mut db::establish_connection();
+    let con = &mut establish_connection();
 
-    let jobs = db::get_jobs(con);
-    for job in jobs {
-        println!("{}", job.id);
+    let jobs = get_jobs(con);
+
+    if jobs.len() > 0 {
+        for job in jobs {}
     }
 }
